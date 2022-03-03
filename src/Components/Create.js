92ccc134c9335f1aafd5a6441abebfdca3ba60b2
prajwal6394 <prajwalpandey2009@gmail.com>
2022-03-04 02:00:02 +0530

    Radio Bottons

--- src/Components/Create.js
+++ src/Components/Create.js
@@ -3,48 +3,104 @@ import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 import Container from "@mui/material/Container";
 import DoubleArrowIcon from "@mui/icons-material/DoubleArrow";
-import { TextField } from "@mui/material";
-import { makeStyles } from "@mui/styles";
+import { FormControlLabel, TextField } from "@mui/material";
+import { useState } from "react";
+import Radio from "@mui/material/Radio";
+import RadioGroup from "@mui/material/RadioGroup";
+import { FormControl } from "@mui/material";
+import { FormLabel } from "@mui/material";
 
-const useStyles = makeStyles({
-  root: {
-    border: "10px solid red",
-    // backgroundColor: 'blue',
-  },
-});
+// const useStyles = makeStyles({
+//   root: {
+//     border: "10px solid red",
+//     // backgroundColor: 'blue',
+//   },
+// });
 export default function Create() {
-  const classes = useStyles();
+  const [title, setTitle] = useState("");
+  const [details, setDetails] = useState("");
+  const [titleError, setTitleError] = useState(false);
+  const [categories, setCategories] = useState("");
+  const [detaileError, setDetailsError] = useState(false);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    setTitleError(false);
+    setDetailsError(false);
+
+    if (title === "") {
+      setTitleError(true);
+    }
+    if (details === "") {
+      setDetailsError(true);
+    }
+    if (title && details) {
+      console.log(title, details, categories);
+    }
+  };
+
   return (
     <Container>
       <Typography
+        style={{ fontFamily: "BhuTuka Expanded One" }}
         variant="h3"
         component="h2"
-        gutterButtom
         color="textSecondary"
       >
         Create a New Note
       </Typography>
 
-      <form noValidate autoComplete="off">
+      <form noValidate autoComplete="off" onSubmit={handleSubmit}>
         <TextField
-          className={classes.root}
+          onChange={(e) => setTitle(e.target.value)}
+          style={{ marginTop: "20px", marginBottom: "20px" }}
           label="Note Here"
           variant="outlined"
           color="secondary"
           fullWidth
           required
+          error={titleError}
         />
+
+        <TextField
+          onChange={(e) => setDetails(e.target.value)}
+          style={{ marginTop: "20px", marginBottom: "20px" }}
+          label="Details"
+          variant="outlined"
+          color="secondary"
+          multiline
+          rows={4}
+          fullWidth
+          required
+          error={detaileError}
+        />
+        <FormControl>
+          <FormLabel style={{marginRight : "350px", marginBottom : "10px"}}>Note Categories </FormLabel>
+          <RadioGroup
+            value={categories}
+            onChange={(e) => setCategories(e.target.value)}
+          >
+            <FormControlLabel value="Money" control={<Radio />} label="Money" />
+            <FormControlLabel value="todos" control={<Radio />} label="Todos" />
+            <FormControlLabel
+              value="reminders"
+              control={<Radio />}
+              label="Reminders"
+            />
+            <FormControlLabel value="work" control={<Radio />} label="Work" />
+          </RadioGroup>
+        </FormControl>
+        <Button
+          color="primary"
+          variant="contained"
+          type="submit"
+          endIcon={<DoubleArrowIcon />}
+          style = {{marginTop:"60px"}}
+        >
+          SUBMIT
+        </Button>
       </form>
 
-      <Button
-        className={classes.root}
-        color="primary"
-        variant="contained"
-        onClick={() => console.log("You clicked Here")}
-        endIcon={<DoubleArrowIcon />}
-      >
-        SUBMIT
-      </Button>
       <br />
       {/* <FitbitIcon/>
       <FitbitIcon color = 'secondary' fontSize="large"/>
